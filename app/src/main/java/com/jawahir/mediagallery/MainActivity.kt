7eb1fdfd08eb417c21dd20cde134efebf1ddd67f
CacheCruncher <jawahir.abul@gmail.com>
2025-03-02 17:52:19 +0400

    [media-permission 2]

--- app/src/main/java/com/jawahir/mediagallery/MainActivity.kt
+++ app/src/main/java/com/jawahir/mediagallery/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -18,45 +19,65 @@ import com.jawahir.mediagallery.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var permissionResultLauncher: ActivityResultLauncher<Array<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        requestPermission()
+        initViews()
+    }
+
+    private fun initViews() {
         binding.permissionReqButton.setOnClickListener {
             openAppSettings()
         }
+        permissionResultLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { perms ->
+            handlePermissionResults(perms)
+        }
+
+        permissionRequestLauncher()
     }
 
-    private fun requestPermission() {
+    private fun permissionRequestLauncher() {
+        val permissionsToRequest = permissions()
+        permissionResultLauncher.launch(permissionsToRequest)
+    }
+
+    private fun permissions(): Array<String> {
         val permissionsToRequest = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             arrayOf(
-                Manifest.permission.READ_MEDIA_IMAGES,
-                Manifest.permission.READ_MEDIA_VIDEO
+                Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO
             )
         } else {
             arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
-
-        val permissionResultLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { perms ->
-            permissionsToRequest.forEach { permission ->
-                onPermissionResult(permission, perms[permission] == true)
-            }
-        }
-
-        permissionResultLauncher.launch(permissionsToRequest)
+        return permissionsToRequest
     }
 
-    private fun onPermissionResult(permission: String, isGranted: Boolean) {
-        if (isGranted) {
+
+    private fun handlePermissionResults(perms: Map<String, Boolean>) {
+        val allGranted = perms.all { it.value }
+
+        if (allGranted && perms.isNotEmpty()) {
             initView()
         } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-                showPermissionDialog()
-            }
+            val deniedPermissions = perms.filter { !it.value }.keys
+            handleDeniedPermissions(deniedPermissions.toList())
+        }
+    }
+
+    private fun handleDeniedPermissions(deniedPermissions: List<String>) {
+        if (deniedPermissions.any {
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, it
+                )
+            }) {
+            showPermissionDialog()
+        } else {
+            showSetting()
         }
     }
 
@@ -72,17 +93,22 @@ class MainActivity : AppCompatActivity() {
         AlertDialog.Builder(this, R.style.AlertDialogTheme_MediaGallery)
             .setTitle(getString(R.string.media_permission_title))
             .setMessage(getString(R.string.media_permission_description))
-            .setPositiveButton(getString(R.string.allow)) { _, _ -> requestPermission() }
-            .setNegativeButton(getString(R.string.cancel),null)
-            .setCancelable(false)
-            .show()
+            .setPositiveButton(getString(R.string.allow)) { _, _ -> permissionRequestLauncher() }
+            .setNegativeButton(getString(R.string.cancel)) { _, _ -> showSetting() }
+            .setCancelable(false).show()
 
+    }
+
+    private fun showSetting() {
+        with(binding) {
+            permissionReqButton.isVisible = true
+            navHostFragment.isVisible = false
+        }
     }
 
     private fun openAppSettings() {
         Intent(
-            ACTION_APPLICATION_DETAILS_SETTINGS,
-            Uri.fromParts("package", packageName, null)
+            ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", packageName, null)
         ).also(::startActivity)
     }
 }
---
+++ app/src/main/java/com/jawahir/mediagallery/ui/uimodels/MediaPermissionUIModel.kt
@@ -0,0 +1,3 @@
+package com.jawahir.mediagallery.ui.uimodels
+
+data class MediaPermissionUIModel(val isPermissionProvided:Boolean)
